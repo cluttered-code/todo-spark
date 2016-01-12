379f3d536bc48e640c0b3171fe4ab74407709793
David Clutter <cluttered.code@gmail.com>
2016-01-12 15:23:07 -0700

    print scp output

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 
 if [[ "$TRAVIS_TAG" == "" ]]
 then
-  export SSHPASS=$DAVID_PASS
-  echo "SSHPAS:" $SSHPASS
-  sshpass -e scp target/todo-spark.jar $DAVID_USER@$DAVID_URL:~/todo-spark-dev
+  output=$(sshpass -p $DAVID_PASS scp target/todo-spark.jar $DAVID_USER@$DAVID_URL:~/todo-spark-dev)
+  echo "$output"
 else
   echo "TAG Build"
 fi
