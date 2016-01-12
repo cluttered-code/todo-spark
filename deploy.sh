0b451424b6826e471e005ac65db043feeb0fd72d
David Clutter <cluttered.code@gmail.com>
2016-01-12 15:47:38 -0700

    more of the same

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 if [[ "$TRAVIS_TAG" == "" ]]
 export SSHPASS=$PASS
 then
-  destination="$USER@$URL:/home/$USER/todo-spark-dev"
-  echo destination
-  sshpass -e scp target/todo-spark.jar $USER@$URL:/home/$USER/todo-spark-dev
+  export destination="$USER@$URL:/home/$USER/todo-spark-dev"
+  echo $destination
+  sshpass -e scp target/todo-spark.jar $destination
 else
   echo "TAG Build"
 fi
