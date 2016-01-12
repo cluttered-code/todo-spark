f5bc2fd3c09fff4a91e135e4b043895a8c0df74a
David Clutter <cluttered.code@gmail.com>
2016-01-12 15:17:37 -0700

    log pass

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 if [[ "$TRAVIS_TAG" == "" ]]
 then
   export SSHPASS=$DAVID_PASS
+  echo "SSHPAS:" $SSHPASS
   sshpass -e scp target/todo-spark.jar $DAVID_USER@$DAVID_URL:~/todo-spark-dev
 else
   echo "TAG Build"
