ff4361b8621454c4252ff5cc36cf702da14c180f
David Clutter <cluttered.code@gmail.com>
2016-01-12 15:03:53 -0700

    travis push jar to dev server

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 if [[ "$TRAVIS_TAG" == "" ]]
 then
-  echo "DEV Build"
+  export SSHPASS=$DAVID_PASS
+  sshpass -e scp ./target/todo-spark.jar $DAVID_USER@$DAVID_URL:/todo-spark-dev
 else
   echo "TAG Build"
 fi
