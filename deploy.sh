308dd453ffb4b114cfe2e5a1198578672f2917d7
David Clutter <cluttered.code@gmail.com>
2016-01-12 22:44:04 -0700

    ummm

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,10 @@ else
 fi
 
 sshpass -e scp -o stricthostkeychecking=no target/todo-spark.jar $USER@$HOST:$DEPLOY_PATH
-sshpass -e ssh $USER@$HOST <<
+
+sshpass -e ssh $USER@$HOST
   cd $DEPLOY_PATH
   if [[ -e "todo-spark.pid" ]]; then echo "Killing running instance"; kill -TERM $(cat todo-spark.pid); fi
   nohup java -jar todo-spark.jar &
   echo \$! > todo-spark.pid
-EOF
-
-exit 0
+exit
