8cbd9c6d97d770ff95caae1e2bb575d3b7151703
David Clutter <cluttered.code@gmail.com>
2016-01-12 22:05:37 -0700

    deploy and start in diff thread

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ fi
 sshpass -e scp -o stricthostkeychecking=no target/todo-spark.jar $USER@$HOST:$DEPLOY_PATH
 sshpass -e ssh $USER@$HOST << EOF
   cd $DEPLOY_PATH
-  java -jar todo-spark.jar
+  java -jar todo-spark.jar &
   \$! > todo-spark.pid
 EOF
 
