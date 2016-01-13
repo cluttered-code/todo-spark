41e79dd8fce37bd46fc5bc657a220ebabaec9e75
David Clutter <cluttered.code@gmail.com>
2016-01-12 21:35:27 -0700

    The variables name was PATH ?!

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ export SSHPASS=$PASS
 if [[ "$TRAVIS_TAG" == "" ]]
 then
   echo "DEV Build"
-  export PATH=$DEV_PATH
+  export DEPLOY_PATH=$DEV_PATH
 else
   echo "TAG Build"
-  export PATH=$PROD_PATH
+  export DEPLOY_PATH=$PROD_PATH
 fi
+
+sshpass -e scp -o stricthostkeychecking=no target/todo-spark.jar $USER@$HOST:$DEPLOY_PATH
 
 exit 0
